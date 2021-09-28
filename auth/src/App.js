7b9e17eb9366c73a70bc21d3904ccfa340481482
Heath Shurtleff <heath.shurtleff@overstock.com>
2021-09-28 16:49:20 -0600

    Fake sign in state management

--- auth/src/App.js
+++ auth/src/App.js
@@ -4,19 +4,25 @@ import { StylesProvider, createGenerateClassName } from '@material-ui/core/style
 
 import Signin from './components/Signin';
 import Signup from './components/Signup';
+import SignIn from './components/Signin';
+import SignUp from './components/Signup';
 
 const generateClassName = createGenerateClassName({
     productionPrefix: 'auth'
 });
 
-export default ({ history }) => {
+export default ({ history, onSignIn }) => {
     return (
         <div>
             <StylesProvider generateClassName={generateClassName}>
                 <Router history={history}>
                     <Switch>
-                        <Route path="/auth/signin" component={Signin} />
-                        <Route path="/auth/signup" component={Signup} />
+                        <Route path="/auth/signin">
+                            <SignIn onSignIn={onSignIn} />
+                        </Route>
+                        <Route path="/auth/signup">
+                            <SignUp onSignIn={onSignIn} />
+                        </Route>
                     </Switch>
                 </Router>
             </StylesProvider>
